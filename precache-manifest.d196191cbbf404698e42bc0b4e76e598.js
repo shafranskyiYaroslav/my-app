d5720b03ed56e334506f1dbfe807052f5bc4b659
ShafranskyiYaroslav <yshafranskyi@intellias.com>
2019-10-22 11:56:05 +0300

    Updates

---
+++ precache-manifest.d196191cbbf404698e42bc0b4e76e598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3df27cdf6e357a9a094956f97b01e32",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "328951ae8a9cbb6c9df4",
+    "url": "/my-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "84e51faba43502e1b7e3",
+    "url": "/my-app/static/css/main.7a649030.chunk.css"
+  },
+  {
+    "revision": "328951ae8a9cbb6c9df4",
+    "url": "/my-app/static/js/2.a7401349.chunk.js"
+  },
+  {
+    "revision": "84e51faba43502e1b7e3",
+    "url": "/my-app/static/js/main.3aa0e112.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
